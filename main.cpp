6097e6b8ec8a52684cd7eddec132e37f58dceb37
irfan <i.abulizi@gothammgt.ca>
2024-03-06 21:06:59 -0500

    use generic compare func

--- main.cpp
+++ main.cpp
@@ -2,28 +2,25 @@
 #include <list>
 #include <chrono>
 
-template<typename Iter>
-using value_type_t = typename std::iterator_traits<Iter>::value_type;
-
-template<typename Iterator, typename Comparator = std::less<value_type_t<Iterator>>>
-void insertionSortSTL(const Iterator& begin, const Iterator& end, Comparator lessThan = Comparator{}) {
+template<typename Iter, typename Comparator>
+void insertionSortSTL(const Iter begin, const Iter end, Comparator compare) {
     for (auto iter = begin; iter != end; iter++) {
-        auto const insertion = std::upper_bound(begin, iter, *iter, lessThan);
+        auto const insertion = std::upper_bound(begin, iter, *iter, compare);
         std::rotate(insertion, iter, std::next(iter));
     }
 }
 
-template<typename Iterator, typename Comparator = std::less<value_type_t<Iterator>>>
-void insertionSort(const Iterator& begin, const Iterator& end, Comparator lessThan = Comparator{}) {
+template<typename Iter, typename Comparator>
+void insertionSort(const Iter begin, const Iter end, Comparator compare) {
     // if the container is empty
     if (begin == end) {
         return;
     }
 
-    for (Iterator j = begin; j != end; j++) {
+    for (Iter j = begin; j != end; j++) {
         auto key = std::move(*j); // move the value to be inserted
-        Iterator i = j;
-        while (i != begin && lessThan(key, *(std::prev(i)))) {
+        Iter i = j;
+        while (i != begin && compare(key, *(std::prev(i)))) {
             *i = std::move(*(std::prev(i)));
             i--;
         }
@@ -35,25 +32,25 @@ void insertionSort(const Iterator& begin, const Iterator& end, Comparator lessTh
 // --------------------------------------------------------------------------------------------------------
 // the blow versions is from the c++ text book
 // --------------------------------------------------------------------------------------------------------
-
 // inner for loop version from the c++ text book in insertionSort chapter
-template<typename Iterator, typename Comparator = std::less<value_type_t<Iterator>>>
-void insertionSortInnerForLoop(const Iterator& begin, const Iterator& end, Comparator lessThan = Comparator{}) {
+template<typename Iter, typename Comparator>
+void insertionSortInnerForLoop(const Iter begin, const Iter end, Comparator compare) {
     if (begin == end) {
         return;
     }
 
-    Iterator i;
+    Iter i;
 
-    for (Iterator j = begin; j != end; j++) {
+    for (Iter j = begin; j != end; j++) {
         auto key = std::move(*j); // move the value to be inserted
-        for (i = j; i != begin && lessThan(key, *(prev(i))); i--) {
+        for (i = j; i != begin && compare(key, *(prev(i))); i--) {
             *i = std::move(*(std::prev(j)));
         }
         *i = std::move(key);
     }
 }
 
+// helper function to print list
 template<typename Container>
 void printList(Container& container) {
     for (const auto& element : container) {
@@ -64,18 +61,19 @@ void printList(Container& container) {
 
 // timing test
 void testRandom() {
-    std::list<int> myList;
+    std::list<int> l;
     std::cout << "Experiment 3: Random Order\n";
     for (int i = 0; i < 10000; ++i) {
-        myList.push_back(rand() % 10000);
+        l.push_back(rand() % 10000);
     }
     auto start = std::chrono::high_resolution_clock::now();
-    insertionSortSTL(myList.begin(), myList.end());
+    insertionSort(l.begin(), l.end(), std::less<int>());
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n\n";
 }
 
 int main() {
+    std::list<int> l = {5,2,1,8,9,3};
     testRandom();
     return 0;
 }
